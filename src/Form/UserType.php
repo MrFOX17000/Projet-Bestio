aea31a5987db6412cdd7153c9da99fd6d36244d9
Hajar <hajarayaddouz29@gmail.com>
2025-09-01 17:18:09 +0200

    test

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
@@ -19,7 +20,7 @@ class UserType extends AbstractType
                     'autofocus' => true,
                 ],
             ])
-            ->add('password')
+            ->add('password', PasswordType::class)
         ;
     }
 
