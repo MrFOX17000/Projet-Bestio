e92bbf30f8e60d6f57bc40d14b6c12e63b625eed
renar3d <renar3d@gmail.com>
2025-09-01 15:36:26 +0200

    Mise en place formumaire de co

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\UserType;
 
 final class SecurityController extends AbstractController
 {
     #[Route('/connexion', name: 'connexion')]
     public function connexion(): Response
     {
+        $form = $this->createForm(UserType::class);
+
         return $this->render('security/connexion.html.twig', [
-            'controller_name' => 'SecurityController',
+            'form' => $form,
         ]);
     }
 }
