469c94b28c6d171b65341b38dcf1f382343d55a0
renar3d <renar3d@gmail.com>
2025-09-03 17:29:54 +0200

    connexion directe après inscription

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,9 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Form\UserType;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Authenticator\FormLoginAuthenticator;
 
 final class SecurityController extends AbstractController
 {
+
     #[Route('/connexion', name: 'connexion')]
     public function connexion(AuthenticationUtils $authenticationUtils): Response
     {
