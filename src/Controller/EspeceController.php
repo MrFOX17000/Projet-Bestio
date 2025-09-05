6451a4fb88747b937f8dbd012cac2edcdbb71d5f
Hajar <hajarayaddouz29@gmail.com>
2025-09-05 11:00:09 +0200

    modif des especes

--- src/Controller/EspeceController.php
+++ src/Controller/EspeceController.php
@@ -84,6 +84,57 @@ final class EspeceController extends AbstractController
         ]);
     }
 
+    #[Route('/edit/espece/{id}', name: 'edit_espece')]
+public function editEspece(
+    int $id,
+    Request $request,
+    EntityManagerInterface $entityManager,
+    EspeceRepository $especeRepository
+): Response
+{
+    // On récupère l'espece existante
+    $espece = $especeRepository->find($id);
+
+    if (!$espece) {
+        $this->addFlash('error', 'Cette espèce n\'existe pas.');
+        return $this->redirectToRoute('app_classe');
+    }
+
+    // On crée le formulaire en injectant l'entité existante
+    $formEspece = $this->createForm(EspeceType::class, $espece, [
+        'method' => 'POST',
+    ]);
+    $formEspece->handleRequest($request);
+
+    if ($formEspece->isSubmitted() && $formEspece->isValid()) {
+        // Gestion de l'image si un nouveau fichier est uploadé
+        $image = $formEspece->get('image')->getData();
+        if ($image) {
+            $imageDirectory = $this->getParameter('images_directory');
+            $newFilename = uniqid() . '.' . $image->guessExtension();
+
+            try {
+                $image->move($imageDirectory, $newFilename);
+                $espece->setImage('/uploads/' . $newFilename);
+            } catch (FileException $e) {
+                $this->addFlash('error', 'Erreur lors de l\'upload de l\'image : ' . $e->getMessage());
+                return $this->redirectToRoute('edit_espece', ['id' => $id]);
+            }
+        }
+        // Si aucune image n'est uploadée, on garde l'image existante
+
+        $entityManager->flush();
+        $this->addFlash('success', 'Espèce modifiée avec succès !');
+        return $this->redirectToRoute('edit_espece', ['id' => $id]);
+    }
+
+    return $this->render('espece/edit.html.twig', [
+        'formEspece' => $formEspece->createView(),
+        'espece' => $espece
+    ]);
+}
+
+
     #[Route('/delete/{id}', name: 'delete_espece')]
     public function delete(Request $request, EntityManagerInterface $entityManager, EspeceRepository $especeRepository, int $id): Response
     {
