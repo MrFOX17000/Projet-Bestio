8e4fbd25dde73c33f7264f05298d60b2efa3a156
Hajar <hajarayaddouz29@gmail.com>
2025-10-06 12:07:37 +0200

    test

--- src/Service/BanManager.php
+++ src/Service/BanManager.php
@@ -13,12 +13,12 @@ class BanManager
         $this->entityManager = $entityManager;
     }
 
-    public function checkAndUnban(User $user): void
+    public function checkAndUnban(User $user): void //Fonction pour débannir automatiqueemnt un utilisateur
     {
-        if ($user->isBanned() && $user->getBannedUntil() !== null) {
+        if ($user->isBanned() && $user->getBannedUntil() !== null) { //on vérifie si l'utilisateur est banni et si la date de fin de ban est définie
             $now = new \DateTime();
-            if ($user->getBannedUntil() < $now) {
-                $user->setBanned(false);
+            if ($user->getBannedUntil() < $now) { //on compare la date actuelle avec la date de fin de ban
+                $user->setBanned(false); //si la date de fin de ban est dépassée, on débannit l'utilisateur
                 $user->setBannedUntil(null);
                 $this->entityManager->flush();
             }
